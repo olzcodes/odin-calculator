9a4008ba8425d4a810259fc6aaf55bc84642fb3b
Oliver L <oliver.lkm@gmail.com>
2023-02-10 22:59:08 +1100

    Create logic for basic operations

--- script.js
+++ script.js
@@ -1,40 +1,65 @@
-const add = function (number1, number2) {
-  return number1 + number2;
+const operate = {
+  add: function (number1, number2) {
+    return number1 + number2;
+  },
+  subtract: function (number1, number2) {
+    return number1 - number2;
+  },
+  multiply: function (number1, number2) {
+    return number1 * number2;
+  },
+  divide: function (number1, number2) {
+    return number1 / number2;
+  },
 };
 
-const subtract = function (number1, number2) {
-  return number1 - number2;
-};
+let displayValue = "";
+let number1;
+let number2;
+let operator;
+let result;
 
-const multiply = function (number1, number2) {
-  return number1 * number2;
-};
-
-const divide = function (number1, number2) {
-  return number1 / number2;
-};
-
-const operate = function (operator, number1, number2) {
-  return operator(number1, number2);
+const clear = function () {
+  displayValue = "";
+  updateDisplayValue(0);
+  displayValue = "";
+  number1 = 0;
+  number2 = 0;
+  operator = "";
 };
 
 const displayEl = document.querySelector(".display");
 
-let displayValue = "";
-
 const updateDisplayValue = function (number) {
   displayValue = displayValue + number;
-  // console.log(displayValue);
   displayEl.textContent = displayValue;
 };
 
 const buttonsContainer = document.querySelector(".buttons-container");
 
 buttonsContainer.addEventListener("click", function (e) {
-  console.log(e.target.textContent);
-  console.log(e.target.classList.value.split(" ")[1]);
   const clicked = e.target.classList.value.split(" ")[1];
   if (clicked === "number") {
     updateDisplayValue(e.target.textContent);
+  }
+  if (clicked === "operator") {
+    number1 = displayValue;
+    console.log(number1);
+    displayValue = "";
+    operator = e.target.id;
+    console.log(operator);
+  }
+  if (clicked === "equal") {
+    number2 = displayValue;
+    console.log(number2);
+    console.log(e.target.classList.value.split(" ")[1]);
+    result = operate[operator](parseInt(number1), parseInt(number2));
+    console.log(result);
+    displayValue = "";
+    updateDisplayValue(result);
+  }
+  if (clicked === "clear") {
+    console.log(e.target.classList.value.split(" ")[1]);
+    clear();
   }
 });
